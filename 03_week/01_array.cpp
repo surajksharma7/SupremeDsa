f1e35623d0a62292500d036fba69c37e69c4b2c8
Suraj Kumar <surajksharma2304@gmail.com>
2024-09-14 15:16:45 +0530

    initial commit

---
+++ 03_week/01_array.cpp
@@ -0,0 +1,16 @@
+// Creating array or array declaration
+
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+ 
+  bool arr[10];
+  char arr_1[15];
+  int arr_2;
+  string arr_3[100];
+  int arr_9[] = {1,2,3,4}; 
+  int arr1[5] = {10,20,30,40,50};
+ return 0;
+}
--- 03_week/02_array.cpp
+++ 03_week/02_array.cpp
@@ -1,16 +1,13 @@
-// Creating array or array declaration
-
 #include<iostream>
 using namespace std;
  
 int main()
 {
- 
-  bool arr[10];
-  char arr_1[15];
-  int arr_2;
-  string arr_3[100];
-  int arr_9[] = {1,2,3,4}; 
-  int arr1[5] = {10,20,30,40,50};
+ int arr[10];
+
+   int crr[] = {10,20,30,40,70};
+   int drr[4] = {0};
+   cout<< drr[0] << endl;
+   cout<< drr[0] << endl;
  return 0;
 }
