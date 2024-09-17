3d36682e81eaa70d5f2dc19a99def7c55c8ee5af
Suraj <surajksharma2304@gmail.com>
2024-09-17 19:41:28 +0530

    initial commit

--- 03_week/14_vector.cpp
+++ 03_week/14_vector.cpp
@@ -12,6 +12,9 @@ int main() {
     vector<int>num;
     num.push_back(10);
     num.push_back(50);
+    num.push_back(19);
     print(num);
+    cout<<num.size()<<" "<<num.capacity();
+
     return 0;
 }
