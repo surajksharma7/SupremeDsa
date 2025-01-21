93c03f88d87ca279411c5f5da0b4c502dd5dd7a4
surajksharma7 <surajksharma2304@gmail.com>
2025-01-21 08:19:23 +0530

    initial commit

---
+++ 05 sorting/2_Sorting.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    
+    vector<int>arr = {50,60,70,10,20,30,40};
+    int start = 0;
+    int end = arr.size() -1;
+    int mid = st/art + (end-start)/2;
+    cout << mid;
+    cout << " " << arr[mid] << endl;
+    return 0;
+}
--- 05 sorting/tempCodeRunnerFile.cpp
+++
@@ -1,4 +0,0 @@
-    cout << "after sprting : ";
-    for(auto v:arr){
-        cout << v << " ";
-    }
